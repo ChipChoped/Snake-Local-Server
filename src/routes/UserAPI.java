660ee6e41d18e821f51c1e31a1e4ba2482555f1f
Chaïmaâ Touhami <chaimaa-touhami@outlook.fr>
2023-02-24 19:35:56 +0100

    Asks the logins API to check the user logins and get their ID

---
+++ src/routes/UserAPI.java
@@ -0,0 +1,58 @@
+package routes;
+
+import org.json.JSONObject;
+import utils.Response;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class UserAPI {
+    private static final String URL = "http://localhost:8080/Snake/rest/user";
+
+    public static Response loginPOST(String username, String password) throws IOException {
+        URL obj = new URL(URL);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Type", "application/json");
+        con.setRequestProperty("Accept", "application/json");
+        con.setDoOutput(true);
+
+        String jsonInputString = "{\"username\": \"" + username + "\", \"password\": \"" + password + "\"}";
+
+        try(OutputStream os = con.getOutputStream()) {
+            byte[] input = jsonInputString.getBytes(StandardCharsets.UTF_8);
+            os.write(input, 0, input.length);
+        }
+
+        int responseCode = con.getResponseCode();
+        String responseLine = null;
+
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            try (BufferedReader br = new BufferedReader(
+                    new InputStreamReader(con.getInputStream(), StandardCharsets.UTF_8))) {
+                StringBuilder response = new StringBuilder();
+                while ((responseLine = br.readLine()) != null) {
+                    response.append(responseLine.trim());
+                }
+
+                return new Response(con.getResponseCode(), new JSONObject(response.toString()));
+            }
+        }
+        else {
+            try (BufferedReader br = new BufferedReader(
+                    new InputStreamReader(con.getErrorStream(), StandardCharsets.UTF_8))) {
+                StringBuilder response = new StringBuilder();
+                while ((responseLine = br.readLine()) != null) {
+                    response.append(responseLine.trim());
+                }
+
+                return new Response(con.getResponseCode(), new JSONObject(response.toString()));
+            }
+        }
+    }
+}
